778e746ce5397163a84669a2c13aec44d2f62087
verdi <87766711+verydianT@users.noreply.github.com>
2021-11-17 04:14:28 +0700

    Add files via upload

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,46 +1,23 @@
 import React from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/splide/dist/css/splide.min.css';
 
 
-const Slide = () => {
+const Slide = ({src}) => {
     return (
       <Splide
         options={{
           perPage: 3,
           rewind: true,
-          gap: "1rem"
+          gap: "1rem",
         }}
       >
-        <SplideSlide>
-          <img
-            src="https://images.unsplash.com/photo-1523429936910-42ccc0e35bc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-            alt="Image 1"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <img
-            src="https://images.unsplash.com/photo-1523429936910-42ccc0e35bc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-            alt="Image 2"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <img
-            src="https://images.unsplash.com/photo-1523429936910-42ccc0e35bc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-            alt="Image 3"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <img
-            src="https://images.unsplash.com/photo-1523429936910-42ccc0e35bc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-            alt="Image 8"
-          />
-        </SplideSlide>
-        <SplideSlide>
-          <img
-            src="https://images.unsplash.com/photo-1523429936910-42ccc0e35bc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
-            alt="Image 8"
-          />
-        </SplideSlide>
+        
+        {src.map((data) => (
+          <SplideSlide>
+            <img src={data.image}  alt={data.alt}/>
+          </SplideSlide>
+        ))}
       </Splide>
     );
   };
