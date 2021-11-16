71da4f1a68638e535baf9fb36653dc0ad5901c5f
verdi <87766711+verydianT@users.noreply.github.com>
2021-11-16 23:37:43 +0700

    Add files via upload

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Card }from "react-bootstrap"
 
-export default function CardCustom(props) {
+const CardCustom = ({imageUrl, title}) => {
     return (
-        <Card style={{ width: '18rem' }}>
-        {/* <Card.Img variant="top" src={props.data.imageUrl} /> */}
-        <Card.Body>
-            <Card.Title style={{textAlign: "center"}}>Card Title</Card.Title>
-        </Card.Body>
+        <Card>
+            <Card.Img src={imageUrl} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+            </Card.Body>
         </Card>
     )
 }
+
+export default CardCustom;
