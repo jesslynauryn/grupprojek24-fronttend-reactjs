7959d0abef6954a1833a0f080e5e46235fd3c644
jesslynauryn <jesslynauryn@rocketmail.com>
2021-11-17 10:26:55 +0700

    coba resolve overwritten

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NaviBar from "./components/Navbar";
 import CardCustom from "./components/Card";
 import ScrollToTop from "./components/ScrollToTop";
-import Slide from "./components/Slider";
-import "@splidejs/splide/dist/css/splide.min.css";
+// import Slide from "./components/Slider";
+// import "@splidejs/splide/dist/css/splide.min.css";
 
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
       <ButtonItem></ButtonItem>
       <CardCustom></CardCustom>
       <ScrollToTop></ScrollToTop>
-      <Slide />
+      {/* <Slide /> */}
     </div>
   );
 }
