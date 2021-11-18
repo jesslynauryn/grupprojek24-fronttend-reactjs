a04b81c0ec116f06346279153fc218f8be887372
jesslynauryn <jesslynauryn@rocketmail.com>
2021-11-19 01:46:47 +0700

    slider, link router

--- src/App.js
+++ src/App.js
@@ -6,24 +6,29 @@ import ScrollToTop from "./components/ScrollToTop";
 import "@splidejs/splide/dist/css/splide.min.css";
 import LandingPage from "./page/LandingPage"
 import Gallery from "./page/Gallery";
-import Slider from "./page/Slider";
+import Slider from "./components/Slider";
 import Content from "./page/Content";
 import Wisata1 from "./page/Wisata1";
 import Comment from "./components/Comment";
 
+import { Routes, Route, Link } from 'react-router-dom';
+import Kuliner from "./page/kuliner"
+import WelcomePage from "./page/welcomePage";
+
+
+
 function App() {
   return (
+  
     <div>
-      <NaviBar></NaviBar>
-      <LandingPage></LandingPage>
-      <Content></Content>
-      <Slider></Slider>
-      <Gallery></Gallery>
-      <Comment />
-
-      {/* -------------------page wisata-1--------------------------------------------- */}
-      {/* <Wisata1 /> */}
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/home" element={<LandingPage />}/>
+        <Route path="/wisata1" element={<Wisata1/>}/> 
+      </Routes>
+     
     </div>
+   
   );
 }
 
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,10 +2,11 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
 const Carousell = ({ carouselData }) => {
+  console.log(carouselData)
   return (
     <Carousel>
       {carouselData.map((data) => (
-        <Carousel.Item>
+          <Carousel.Item>
           {data.content}
           <Carousel.Caption>
             <h3>{data.title}</h3>
