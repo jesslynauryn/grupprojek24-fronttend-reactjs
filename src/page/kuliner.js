3ca412b942c0b4445f1fe204622ab1cb9269aba2
revydo <eldodo641@gmail.com>
2021-11-19 01:28:53 +0700

    undelete file

---
+++ src/page/kuliner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+const Kuliner =()=>{
+    return(
+        <div>
+            <Row>
+                <Col>
+                    <Image src="https://images.unsplash.com/photo-1636972708846-7988b903a53c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=850&q=80" fluid />
+                    <h3>Sate Lilit</h3>
+                    <p>Eksplore kuliner khas Bali</p>
+                </Col>
+            </Row>
+        </div>
+    )
+};
